e96371783bc674a0ad8f7c69252b6ba883714cfa
= <=>
2021-01-16 18:31:15 +0700

    reponsive complete

--- asset/js/main.js
+++ asset/js/main.js
@@ -70,3 +70,24 @@ window.addEventListener('scroll', scrollTop)
 
 
 
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 1800,
+    reset: true,
+});
+
+sr.reveal(`.home__data, .home__img, 
+           .decoration__data,
+           .accessory__content,
+           .footer__content`, {
+    origin: 'top',
+    interval: 200,
+})
+
+sr.reveal(`.share__img, .send__content`, {
+    origin: 'left'
+})
+
+sr.reveal(`.share__data, .send__img`, {
+    origin: 'right'
+})
